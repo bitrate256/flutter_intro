1f9535b41e0f5940f8ccfc63e35e26309d57511d
bitrate256 <bitrate256@gmail.com>
2022-07-13 23:12:51 +0900

    실습예제 기초구조

---
+++ flutter_miso_starbucks/lib/miso.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+/// Miso 메인 색상
+Color misoPrimaryColor = Color.fromARGB(255, 38, 103, 240);
+
+class Miso extends StatefulWidget {
+  const Miso({Key? key}) : super(key: key);
+
+  @override
+  _MisoState createState() => _MisoState();
+}
+
+class _MisoState extends State<Miso> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex, // index 순서에 해당하는 child를 맨 위에 보여줌
+        children: [
+          MisoFirstPage(),
+          MisoSecondPage(),
+          MisoThirdPage(),
+          MisoFourthPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex, // 현재 보여주는 탭
+        onTap: (newIndex) {
+          print("selected newIndex : $newIndex");
+          // 다른 페이지로 이동
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        selectedItemColor: misoPrimaryColor, // 선택된 아이콘 색상
+        unselectedItemColor: Colors.grey, // 선택되지 않은 아이콘 색상
+        showSelectedLabels: false, // 선택된 항목 label 숨기기
+        showUnselectedLabels: false, // 선택되지 않은 항목 label 숨기기
+        type: BottomNavigationBarType.fixed, // 선택시 아이콘 움직이지 않기
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.redeem), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        ],
+      ),
+    );
+  }
+}
+
+/// 첫 번째 페이지
+class MisoFirstPage extends StatelessWidget {
+  const MisoFirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 첫 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 두 번째 페이지
+class MisoSecondPage extends StatelessWidget {
+  const MisoSecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 두 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 세 번째 페이지
+class MisoThirdPage extends StatelessWidget {
+  const MisoThirdPage({Key? key}) : super(key: key);
+
+  /// 세 번째 화면 배경 이미지 URL
+  final String backgroundImgUrl =
+      "https://i.ibb.co/rxzkRTD/146201680-e1b73b36-aa1e-4c2e-8a3a-974c2e06fa9d.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 세 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 네 번째 페이지
+class MisoFourthPage extends StatelessWidget {
+  const MisoFourthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Miso 네 번째 페이지"),
+      ),
+    );
+  }
+}
---
+++ flutter_miso_starbucks/lib/starbucks.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+/// Starbucks 메인 색상
+Color starbucksPrimaryColor = Color.fromARGB(255, 83, 184, 138);
+
+/// Starbucs 포인트 색상
+Color starbucksAccentColor = Color.fromARGB(255, 199, 176, 121);
+
+class Starbucks extends StatefulWidget {
+  const Starbucks({Key? key}) : super(key: key);
+
+  @override
+  _StarbucksState createState() => _StarbucksState();
+}
+
+class _StarbucksState extends State<Starbucks> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex, // index 순서에 해당하는 child를 맨 위에 보여줌
+        children: [
+          StarbucksFirstPage(),
+          StarbucksSecondPage(),
+          StarbucksThirdPage(),
+          Center(child: Text("Starbucks 네 번째 페이지")),
+          Center(child: Text("Starbucks 다섯 번째 페이지")),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex, // 현재 보여주는 탭
+        onTap: (newIndex) {
+          print("selected newIndex : $newIndex");
+          // 다른 페이지로 이동
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        selectedItemColor: starbucksPrimaryColor, // 선택된 아이콘 색상
+        unselectedItemColor: Colors.grey, // 선택되지 않은 아이콘 색상
+        showSelectedLabels: false, // 선택된 항목 label 숨기기
+        showUnselectedLabels: false, // 선택되지 않은 항목 label 숨기기
+        type: BottomNavigationBarType.fixed, // 선택시 아이콘 움직이지 않기
+        backgroundColor: Colors.white.withOpacity(0.8),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card_rounded), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.free_breakfast), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.redeem), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: ""),
+        ],
+      ),
+    );
+  }
+}
+
+/// 첫 번째 페이지
+class StarbucksFirstPage extends StatelessWidget {
+  const StarbucksFirstPage({Key? key}) : super(key: key);
+
+  /// 배경 이미지 URL
+  final String backImg =
+      "https://i.ibb.co/2Pz33q7/2021-12-16-12-21-42-cleanup.png";
+
+  /// Frequency 이미지 URL
+  final String frequencyImg = "https://i.ibb.co/QcVn97y/2021-12-16-1-33-11.png";
+
+  /// 추천 메뉴
+  final List<Map<String, String>> recommendMenu = const [
+    {
+      "name": "돌체쿠키라떼",
+      "imgUrl": "https://i.ibb.co/SwGPpzR/9200000003687-20211118142543832.jpg",
+    },
+    {
+      "name": "아이스 홀리데이 돌체 쿠키 라떼",
+      "imgUrl": "https://i.ibb.co/JHVXZ72/9200000003690-20211118142702357.jpg",
+    },
+    {
+      "name": "스노우 민트 초콜릿",
+      "imgUrl": "https://i.ibb.co/M91G17c/9200000003693-20211118142933650.jpg",
+    },
+    {
+      "name": "아이스 스노우 민트 초콜릿",
+      "imgUrl": "https://i.ibb.co/jyZK4C9/9200000003696-20211118143125337.jpg",
+    },
+    {
+      "name": "스노우 민트 초콜릿 블렌디드",
+      "imgUrl": "https://i.ibb.co/DKkV0rw/9200000003699-20211118143249044.jpg",
+    },
+  ];
+
+  /// 크리스마스 이벤트 이미지 URL
+  final String eventImg = "https://i.ibb.co/Fb0q43T/IMG-F9-BA5-CBCB476-1.jpg";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("Starbucks 첫 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 두 번째 페이지
+class StarbucksSecondPage extends StatelessWidget {
+  const StarbucksSecondPage({Key? key}) : super(key: key);
+
+  /// 카드 이미지
+  final String cardImgUrl = "https://i.ibb.co/BgfYHg4/2021-12-16-1-49-51.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Starbucks 두 번째 페이지"),
+      ),
+    );
+  }
+}
+
+/// 세 번째 페이지
+class StarbucksThirdPage extends StatelessWidget {
+  const StarbucksThirdPage({Key? key}) : super(key: key);
+
+  /// 전체 메뉴
+  final List<Map<String, String>> menu = const [
+    {
+      "ko": "추천",
+      "en": "Recommend",
+      "imgUrl": "https://i.ibb.co/SwGPpzR/9200000003687-20211118142543832.jpg",
+    },
+    {
+      "ko": "리저브 에스프레소",
+      "en": "Reserve Espresso",
+      "imgUrl": "https://i.ibb.co/JHVXZ72/9200000003690-20211118142702357.jpg",
+    },
+    {
+      "ko": "리저브 드립",
+      "en": "Reserve Drip",
+      "imgUrl": "https://i.ibb.co/M91G17c/9200000003693-20211118142933650.jpg",
+    },
+    {
+      "ko": "콜드브루",
+      "en": "ColdBrew",
+      "imgUrl": "https://i.ibb.co/jyZK4C9/9200000003696-20211118143125337.jpg",
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Starbucks 세 번째 페이지"),
+      ),
+    );
+  }
+}
