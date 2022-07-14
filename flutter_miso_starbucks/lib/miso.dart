9807e03b486ad65e4ae0162eaadef859e04783d8
bitrate256 <bitrate256@gmail.com>
2022-07-14 13:03:14 +0900

    미소 예제 화면 완성

--- flutter_miso_starbucks/lib/miso.dart
+++ flutter_miso_starbucks/lib/miso.dart
@@ -58,8 +58,88 @@ class MisoFirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 첫 번째 페이지"),
+      backgroundColor: misoPrimaryColor,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity, // 화면을 가로로 꽉 채워주는 방법
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // 기기 높이의 %로 줘야 각 기기별로 적절한 위치에 배치할 수 있음
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+
+                  // 대한민국 1등 홈서비스 문구
+                  Text(
+                    "대한민국 1등 홈서비스\n미소를 만나보세요!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 38),
+
+                  // + 예약하기 버튼
+                  GestureDetector(
+                    onTap: () {
+                      print("+ 예약하기 버튼 클릭 됨");
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(64),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.add, color: misoPrimaryColor),
+                          SizedBox(width: 6),
+                          Text(
+                            "예약하기",
+                            style: TextStyle(
+                              color: misoPrimaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+              // 서비스 상세정보
+              Positioned(
+                bottom: 32,
+                child: GestureDetector(
+                  onTap: () {
+                    print("서비스 상세정보 클릭 됨");
+                  },
+                  child: Container(
+                    color: Colors.white.withOpacity(0.3),
+                    padding: EdgeInsets.all(12),
+                    child: Text(
+                      "서비스 상세정보",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -72,8 +152,84 @@ class MisoSecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 두 번째 페이지"),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 기기 높이의 %로 줘야 각 기기별로 적절한 위치에 배치할 수 있음
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+
+                    // 예약내역
+                    Text(
+                      "예약내역",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 32,
+                      ),
+                    ),
+                    SizedBox(height: 64),
+
+                    Row(
+                      children: [
+                        // ! 아이콘
+                        Icon(Icons.error, color: misoPrimaryColor),
+                        SizedBox(width: 8),
+
+                        // 예약된 서비스 없음
+                        // 폰의 폭과 관계없이 텍스트를 언제나 한줄로 보여주기
+                        Expanded(
+                          // row의 남은 영역을 모두 차지하도록
+                          child: FittedBox(
+                            // 해당 영역에 적절한 사이즈로 맞추기
+                            fit: BoxFit.scaleDown, // child 최소 크기를 존중
+                            child: Text(
+                              "예약된 서비스가 아직 없어요. 지금 예약해보세요!",
+                              style: TextStyle(
+                                fontSize: 100, // 아무리 커도 한줄로 보임
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Divider(color: Colors.grey),
+                  ],
+                ),
+              ),
+
+              // 예약하기
+              Positioned(
+                bottom: 18,
+                left: 24,
+                right: 24,
+                child: GestureDetector(
+                  onTap: () {
+                    print("예약하기 클릭 됨");
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 58,
+                    color: misoPrimaryColor,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "예약하기",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -90,8 +246,133 @@ class MisoThirdPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 세 번째 페이지"),
+      backgroundColor: misoPrimaryColor,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            // 친구 추천하기 버튼에 width를 주지 않고 중앙 정렬하기
+            alignment: Alignment.center,
+            children: [
+              // 배경 이미지
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  // 이미지 최대 크기 제한
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Image.network(backgroundImgUrl),
+                ),
+              ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 64),
+
+                  // 타이틀
+                  // 10000원만 bold 처리하기 위해 RitchText 위젯 사용
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      // 공통 스타일
+                      style: TextStyle(
+                        fontSize: 28,
+                        height: 1.5, // 줄간격 한줄의 1.5배
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "친구 추천할 때 마다\n",
+                        ),
+                        TextSpan(
+                          text: "10000원",
+                          // 개별 스타일
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "할인 쿠폰 지급!",
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 64),
+
+                  // 자세히 보기
+                  GestureDetector(
+                    onTap: () {
+                      print("자세히 보기 클릭 됨");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "자세히 보기",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // 친구 추천하기
+              Positioned(
+                bottom: 42,
+                child: GestureDetector(
+                  onTap: () {
+                    print("친구 추천하기 클릭 됨");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(64),
+                      // 그림자
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          // 광원의 위치를 중심(0, 0) 보다 위쪽(0.5133)
+                          offset: Offset(0, 5),
+                          spreadRadius: 1,
+                          blurRadius: 12,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.redeem,
+                          color: misoPrimaryColor,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          "친구 추천하기",
+                          style: TextStyle(
+                            color: misoPrimaryColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -104,8 +385,47 @@ class MisoFourthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Miso 네 번째 페이지"),
+      body: SafeArea(
+        // 작은 화면에서 넘친다면 스크롤 가능하도록
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 62,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "나의 정보",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 32,
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "010-9999-9999",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 32),
+              ListTile(
+                onTap: () => print("주소 관리 클릭 됨"),
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.home_outlined, color: Colors.black),
+                title: Text(
+                  "문의사항",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
