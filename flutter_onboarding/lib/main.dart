51d08ba32dc56ee931e44f857f91d92a0d3b515a
bitrate256 <bitrate256@gmail.com>
2022-07-22 14:37:26 +0900

    온보딩 앱 완성 introdution_screen 패키지, shared preferences 패키지

--- flutter_onboarding/lib/main.dart
+++ flutter_onboarding/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+// SharedPreferences 인스턴스를 어디서든 접근 가능하도록 전역 변수로 선언
+late SharedPreferences prefs; // late : 나중에 값을 할당하겠다는 뜻
+
+void main() async {
+    // main() 함수에서 async를 쓰려면 필요
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // shared_preferences 인스턴스 생성
+    prefs = await SharedPreferences.getInstance();
+
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,9 +21,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // SharedPreferences에서 온보딩 완료 여부 조회
+    // isOnboarded에 해당하는 값에서 null을 반환하는 경우 false 할당
+    bool isOnboarded = prefs.getBool("isOnboarded") ?? false; // null 인 경우 false를 반환하도록 ?? false 붙임
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.getTextTheme('Nanum Gothic'),
+      ),
+      home: isOnboarded ? HomePage() : OnboardingPage(),
     );
   }
 }
@@ -22,7 +40,92 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hello OnboardingPage")),
+      body: IntroductionScreen(
+        pages: [
+          // 첫 번째 페이지
+          PageViewModel(
+            title: "빠른 개발",
+            body: "Flutter의 hot reload는 쉽고 UI 빌드를 도와줍니다.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.network(
+                  'https://user-images.githubusercontent.com/26322627/143761841-ba5c8fa6-af01-4740-81b8-b8ff23d40253.png'),
+            ),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          // 두 번째 페이지
+          PageViewModel(
+            title: "표현력 있고 유연한 UI",
+            body: "Flutter에 내장된 아름다운 위젯!",
+            image: Image.network(
+                'https://user-images.githubusercontent.com/26322627/143762620-8cc627ce-62b5-426b-bc81-a8f578e8549c.png'),
+            decoration: PageDecoration(
+              titleTextStyle: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+        next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
+        done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        onDone: () {
+          // Done 클릭시 isOnboarded = true로 저장
+          prefs.setBool("isOnboarded", true);
+
+          // Done 클릭시 페이지 이동 (뒤로가기 없음)
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page!"),
+        actions: [
+          // 삭제 버튼
+          IconButton(
+            onPressed: () {
+              // SharedPreferences에 저장된 모든 데이터 삭제
+              prefs.clear();
+            },
+            icon: Icon(Icons.delete),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "환영합니다!",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
     );
   }
 }
