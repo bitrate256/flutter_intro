a934bdcd49c5e2e57aa91db4948085df6f869b5e
bitrate256 <bitrate256@gmail.com>
2022-08-01 17:43:54 +0900

    provider 예제2 기초작업 1. 패키지 2 추가 (table_calendar/intl) 2. 캘린터 출력 및 오늘/선택날짜 표시 3. CREATE 컨트롤러 생성

--- flutter_crud_provider_2/lib/diary_service.dart
+++ flutter_crud_provider_2/lib/diary_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Diary {
   String text; // 내용
@@ -16,13 +17,31 @@ class DiaryService extends ChangeNotifier {
 
   /// 특정 날짜의 diary 조회
   List<Diary> getByDate(DateTime date) {
-    // TODO : 아래 내용을 지우고 구현해주세요.
-    return diaryList;
+    return diaryList
+        .where((diary) => isSameDay(date, diary.createdAt))
+        .toList();
   }
 
   /// Diary 작성
   void create(String text, DateTime selectedDate) {
-    // TODO
+    DateTime now = DateTime.now();
+
+    // 선택된 날짜(selectedDate)에 현재 시간으로 추가
+    DateTime createdAt = DateTime(
+      selectedDate.year,
+      selectedDate.month,
+      selectedDate.day,
+      now.hour,
+      now.minute,
+      now.second,
+    );
+
+    Diary diary = Diary(
+        text: text,
+        createdAt: createdAt,
+    );
+    diaryList.add(diary);
+    notifyListeners();
   }
 
   /// Diary 수정
--- flutter_crud_provider_2/lib/home_page.dart
+++ flutter_crud_provider_2/lib/home_page.dart
@@ -1,17 +1,79 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_crud_provider_2/diary_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // 달력 보여주는 형식
+  CalendarFormat calendarFormat = CalendarFormat.month;
+
+  // 선택된 날짜
+  DateTime selectedDate = DateTime.now();
+
+  // CREATE text controller
+  TextEditingController createTextController = TextEditingController();
+
+  // UPDATE text controller
+  TextEditingController updateTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Consumer<DiaryService>(
       builder: (context, diaryService, child) {
+        List<Diary> diaryList = diaryService.getByDate(selectedDate);
         return Scaffold(
-          body: Center(
-            child: Text("HomePage"),
+          // 키보드가 올라올 때 화면 밀지 않도록 만들기(overflow 방지)
+          resizeToAvoidBottomInset: false,
+          body: SafeArea(
+            child: Column(
+              children: [
+                /// 달력
+                TableCalendar(
+                  firstDay: DateTime.utc(2010),
+                  lastDay: DateTime.utc(2099),
+                  focusedDay: selectedDate,
+                  calendarFormat: calendarFormat,
+                  onFormatChanged: (format) {
+                    // 달력 형식 변경
+                    setState(() {
+                      calendarFormat = format;
+                    });
+                  },
+                  eventLoader: (date) {
+                    // 각 날짜에 해당하는 diaryList 보여주기
+                    return diaryService.getByDate(date);
+                  },
+                  calendarStyle: CalendarStyle(
+                    // toDay 색상 제거 (투데이는 화이트)
+                    todayTextStyle: TextStyle(color: Colors.black),
+                    todayDecoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  // 선택된 날짜
+                  selectedDayPredicate: (day) {
+                    return isSameDay(selectedDate, day);
+                  },
+                  // 선택된 날짜 색깔
+                  onDaySelected: (_, focusedDay) {
+                    setState(() {
+                      selectedDate = focusedDay;
+                    });
+                  },
+                ),
+                // 화면 나누기 (디바이더)
+                Divider(height: 1),
+              ],
+            ),
           ),
         );
       },
