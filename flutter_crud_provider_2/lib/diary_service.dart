ff8b5894c7f28e9d5f78b078e3d15610aae1fc8d
bitrate256 <bitrate256@gmail.com>
2022-08-10 17:42:20 +0900

    앱을 재시작 해도 데이터가 유지되도록 개선 + SharedPreferences 패키지 추가 + 해당 패키지는 String/List<String>/int/double 타입의 데이터만 저장가능, 직접 만든 Diary클래스를 저장하기 위해 Json 규칙을 따라 String으로 변환

--- flutter_crud_provider_2/lib/diary_service.dart
+++ flutter_crud_provider_2/lib/diary_service.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:table_calendar/table_calendar.dart';
 
 class Diary {
@@ -9,10 +13,46 @@ class Diary {
     required this.text,
     required this.createdAt,
   });
+
+  // Diary -> Map 변경
+  Map<String, dynamic> toJson() {
+    return {
+      "text": text,
+      // DateTime은 문자열로 변경해야 jsonString 으로 변환 가능함
+      "createdAt": createdAt.toString(),
+    };
+  }
+
+  // Map -> Diary 변경
+  factory Diary.fromJson(Map<String, dynamic> jsonMap) {
+    return Diary(
+      text: jsonMap['text'],
+      // 문자열로 넘어온 시간을 DateTime으로 다시 바꿈
+      createdAt: DateTime.parse(jsonMap['createdAt']),
+    );
+  }
 }
 
 class DiaryService extends ChangeNotifier {
-  /// Diary 목록
+  /// 생성자
+  DiaryService(this.preferences) {
+    // 생성자가 호출될 때 SharedPreferences로 저장해둔 diaryList를 불러옴
+    // 저장할 때와 반대로 String -> Map -> Diary 로 변환함
+    List<String> strintDiaryList = preferences.getStringList("diaryList") ?? [];
+    for (String stringDiary in strintDiaryList) {
+      // String -> Map
+      Map<String, dynamic> jsonMap = jsonDecode(stringDiary);
+
+      // Map -> Diary
+      Diary diary = Diary.fromJson(jsonMap);
+      diaryList.add(diary);
+    }
+  }
+
+  // SharedPreferences 인스턴스
+  SharedPreferences preferences;
+
+  // Diary 목록
   List<Diary> diaryList = [];
 
   /// 특정 날짜의 diary 조회
@@ -42,6 +82,9 @@ class DiaryService extends ChangeNotifier {
     );
     diaryList.add(diary);
     notifyListeners();
+
+    // diary 정보가 변경될 때 마다 저장됨
+    _saveDiaryList();
   }
 
   /// Diary 수정
@@ -61,5 +104,23 @@ class DiaryService extends ChangeNotifier {
     // createdAt 이 일치하는 diary 삭제
     diaryList.removeWhere((diary) => diary.createdAt == createdAt);
     notifyListeners();
+  }
+
+  // 변경된 Diary SharedPreferences 로 저장
+  // 함수 이름을 _로 이름을 시작하면 DiaryService 내부에서만 호출할 수 있음
+  void _saveDiaryList() {
+    // Diary라는 직접 만든 클래스는 shared preferences에 그대로 저장할 수 없음
+    // SharedPreferences 에서 저장할 수 있는 String 형태로 변환
+    // 나만의 규칙으로 Diary를 String 형태로 변환할 수 있음
+    List<String> stringDiaryList = [];
+    for(Diary diary in diaryList) {
+      // Diary -> Map
+      Map<String, dynamic> jsonMap = diary.toJson();
+
+      // Map -> String
+      String stringDiary = jsonEncode(jsonMap);
+      stringDiaryList.add(stringDiary);
+    }
+    preferences.setStringList("diaryList", stringDiaryList);
   }
 }
--- flutter_crud_provider_2/lib/main.dart
+++ flutter_crud_provider_2/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'diary_service.dart';
 import 'home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // sharedPreferences 인스턴스 불러오기
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => DiaryService()),
+        ChangeNotifierProvider(create: (context) => DiaryService(preferences)),
       ],
       child: const MyApp(),
     ),
