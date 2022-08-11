530be17af436788d5ad57b300c03432b28f24e9b
bitrate256 <bitrate256@gmail.com>
2022-08-11 14:13:58 +0900

    API 예제 프로젝트2 생성

---
+++ flutter_api_example2/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CatService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+/// 고양이 서비스
+class CatService extends ChangeNotifier {
+  // 고양이 사진 담을 변수
+  List<String> catImages = [];
+}
+
+/// 홈 페이지
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(
+      builder: (context, catService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("랜덤 고양이"),
+            backgroundColor: Colors.amber,
+            actions: [
+              // 좋아요 페이지로 이동
+              IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoritePage()),
+                  );
+                },
+              )
+            ],
+          ),
+          // 고양이 사진 목록
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(
+              10,
+                  (index) {
+                return Center(
+                  child: Text("$index", style: TextStyle(fontSize: 24)),
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// 좋아요 페이지
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(
+      builder: (context, catService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("좋아요"),
+            backgroundColor: Colors.amber,
+          ),
+        );
+      },
+    );
+  }
+}
