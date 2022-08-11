7eeea419e5d37b93efe98fadef6a65fc96ebaa17
bitrate256 <bitrate256@gmail.com>
2022-08-11 18:12:54 +0900

    좋아요 기능 추가 좋아요 클릭 + 좋아요 체크된 리스트

--- flutter_api_example2/lib/main.dart
+++ flutter_api_example2/lib/main.dart
@@ -30,6 +30,9 @@ class CatService extends ChangeNotifier {
   // 고양이 사진 담을 변수
   List<String> catImages = [];
 
+  // 좋아요 사진
+  List<String> favoriteImages = [];
+
   // 생성자(Constructor)
   CatService() {
     getRandomCatImages(); // api 호출
@@ -37,13 +40,25 @@ class CatService extends ChangeNotifier {
 
   // 랜덤 고양이 사진 API 호출
   void getRandomCatImages() async {
-    var result = await Dio().get("https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg");
+    var result = await Dio().get(
+        "https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg");
     print(result.data);
     for (int i = 0; i < result.data.length; i++) {
       var map = result.data[i]; // 반복문을 돌며 i번째의 map에 접근
       print(map);
       print(map['url']); // url만 추출
       catImages.add(map['url']); // catImages에 이미지 추가
+    }
+
+    notifyListeners(); // 새로고침
+  }
+
+  // 좋아요 토글
+  void toggleFavoriteImage(String catImage) {
+    if (favoriteImages.contains(catImage)) {
+      favoriteImages.remove(catImage); // 이미 좋아요한 경우 제거
+    } else {
+      favoriteImages.add(catImage); // 새로운 사진 추가
     }
 
     notifyListeners(); // 새로고침
@@ -83,11 +98,40 @@ class HomePage extends StatelessWidget {
             crossAxisCount: 2,
             children: List.generate(
               catService.catImages.length, // 보여주려는 항목 개수
-                  (index) {
+              (index) {
                 String catImage = catService.catImages[index];
-                return Image.network(
-                  catImage,
-                  fit: BoxFit.cover,
+                return GestureDetector(
+                  onTap: () {
+                    // 사진 클릭시
+                    // print("클릭 $index");
+                    catService.toggleFavoriteImage(catImage);
+                  },
+                  // child: Image.network(
+                  //   catImage,
+                  //   fit: BoxFit.cover,
+                  // ),
+                  child: Stack(
+                    children: [
+                      // 사진
+                      Positioned.fill(
+                        child: Image.network(
+                          catImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // 좋아요
+                      Positioned(
+                        bottom: 8,
+                        right: 8,
+                        child: Icon(
+                          Icons.favorite,
+                          color: catService.favoriteImages.contains(catImage)
+                              ? Colors.amber
+                              : Colors.transparent,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
@@ -110,6 +154,51 @@ class FavoritePage extends StatelessWidget {
           appBar: AppBar(
             title: Text("좋아요"),
             backgroundColor: Colors.amber,
+          ),
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(
+              catService.favoriteImages.length, // 보여주려는 항목 개수
+              (index) {
+                String catImage = catService.favoriteImages[index];
+                return GestureDetector(
+                  onTap: () {
+                    // 사진 클릭시
+                    // print("클릭 $index");
+                    catService.toggleFavoriteImage(catImage);
+                  },
+                  // child: Image.network(
+                  //   catImage,
+                  //   fit: BoxFit.cover,
+                  // ),
+                  child: Stack(
+                    children: [
+                      // 사진
+                      Positioned.fill(
+                        child: Image.network(
+                          catImage,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      // 좋아요
+                      Positioned(
+                        bottom: 8,
+                        right: 8,
+                        child: Icon(
+                          Icons.favorite,
+                          color: catService.favoriteImages.contains(catImage)
+                              ? Colors.amber
+                              : Colors.transparent,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         );
       },
