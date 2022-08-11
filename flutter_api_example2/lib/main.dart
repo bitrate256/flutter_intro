3e886a44ebaa5acabaabdb10b60e045214de868f
bitrate256 <bitrate256@gmail.com>
2022-08-11 16:46:53 +0900

    API 기초 호출 및 패키지 설치

--- flutter_api_example2/lib/main.dart
+++ flutter_api_example2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(
@@ -28,6 +29,25 @@ class MyApp extends StatelessWidget {
 class CatService extends ChangeNotifier {
   // 고양이 사진 담을 변수
   List<String> catImages = [];
+
+  // 생성자(Constructor)
+  CatService() {
+    getRandomCatImages(); // api 호출
+  }
+
+  // 랜덤 고양이 사진 API 호출
+  void getRandomCatImages() async {
+    var result = await Dio().get("https://api.thecatapi.com/v1/images/search?limit=1&mime_types=jpg");
+    print(result.data);
+    for (int i = 0; i < result.data.length; i++) {
+      var map = result.data[i]; // 반복문을 돌며 i번째의 map에 접근
+      print(map);
+      print(map['url']); // url만 추출
+      catImages.add(map['url']); // catImages에 이미지 추가
+    }
+    
+    notifyListeners(); // 새로고침
+  }
 }
 
 /// 홈 페이지
